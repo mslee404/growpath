d3c15b021db278b5ac86743f5ed1a9e9633eef97
mslee404 <risquinaangelicaa@gmail.com>
2025-11-06 10:13:15 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,15 @@ Route::get('/', function () {
 Route::get('/login', function () {
        return view('auth.login');
    })->name('login');
+
+Route::get('/register', function () {
+       return view('auth.register');
+   })->name('register');
    
 Route::get('/loginafter', function () {
        return view('auth.loginafter');
    })->name('loginafter');
 
-Route::get('/register', function () {
-       return view('auth.register');
-   })->name('register');
 
 Route::get('/inventory', function () {
     return view('inventory'); 
